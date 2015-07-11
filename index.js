c191ff6cf101b127be2e4d1de3b6ea04e3600e12
Sean Lavine <freewil@users.noreply.github.com>
2015-07-11 14:32:56 -0700

    initial

---
+++ index.js
@@ -0,0 +1,53 @@
+var app = require('app')
+var Menu = require('menu')
+var Tray = require('tray')
+
+var CoinbaseExchange = require('coinbase-exchange')
+
+// Report crashes to our server.
+// require('crash-reporter').start()
+var publicClient = new CoinbaseExchange.PublicClient()
+
+var TIMEOUT_MS = 60 * 1000
+var firstUpdate = true
+var tray = null
+
+function updatePrice () {
+  publicClient.getProductTicker(function (err, ticker) {
+    if (err) {
+      // TODO eventually do something when this fails
+    } else {
+      // TODO catch error from JSON.parse()
+      var json = JSON.parse(ticker.body)
+      var price = parseFloat(json.price).toFixed(2)
+      tray.setTitle('$' + price)
+    }
+
+    if (firstUpdate) {
+      // immediately set price again on first update to ensure tray width is
+      // set properly - this is probably a glitch in electron's tray module
+      firstUpdate = false
+      process.nextTick(function () {
+        tray.setTitle('$' + price)
+      })
+    }
+
+    setTimeout(updatePrice, TIMEOUT_MS)
+  })
+}
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function () {
+  tray = new Tray(__dirname + '/IconTemplate.png')
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Quit',
+      click: function () {
+        app.quit()
+      }
+    }
+  ])
+  tray.setContextMenu(contextMenu)
+  updatePrice()
+})
